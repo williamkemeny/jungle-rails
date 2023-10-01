08d1dd3bee6ac930a75b6e0c2774f8c794479489
William Kemeny <williamkemeny@gmail.com>
2023-10-01 17:08:24 -0400

    env file updates

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,5 +1,6 @@
 class Admin::DashboardController < ApplicationController
-  http_basic_authenticate_with name: "Jungle", password: "book", except: :index
+  http_basic_authenticate_with name: ENV["ADMIN_NAME"], password: ENV["ADMIN_PASSWORD"]
+  
   def show
   end
 end
